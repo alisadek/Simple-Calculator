a01ee02b8a777a4a731b5b1ddee3f8407d31364f
alisadek <46340800+alisadek@users.noreply.github.com>
2020-01-18 08:00:09 +0200

    Update calculator.h

--- Calculator/calculator.h
+++ Calculator/calculator.h
@@ -1,17 +1,19 @@
-/*
- * calculator.h
+ /******************************************************************************
  *
- *  Created on: Jan 2, 2020
- *      Author: aly_s
- */
-
+ * Module: CALCULATOR
+ *
+ * File Name: calculator.h
+ *
+ * Description: Header file for the Calculator driver
+ *
+ * Author: Ali Sadek
+ *
+ *******************************************************************************/
 #ifndef CALCULATOR_H_
 #define CALCULATOR_H_
 
 #include "keypad.h"
 #include "lcd.h"
 #include <stdlib.h>
-
-#define NUM_LENGTH 16
 
 #endif /* CALCULATOR_H_ */
